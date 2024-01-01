2626960ba1a4eea1e6c87c6fcea8e2157760a7d0
Ethan <QustoTemmo@protonmail.com>
2024-01-01 13:03:57 +0100

    Organisation Complete

--- main.py
+++ main.py
@@ -2,11 +2,36 @@ from flask import Flask, render_template, redirect, request
 from datetime import datetime
 
 app = Flask('__main__')
+copyright_year = datetime.now().year
 
 @app.route('/')
-def index():
-      copyright_year = datetime.now().year
-      return render_template('index.html', year=copyright_year)
+def home():
+      return render_template('home.html', year=copyright_year)
 
-if '__name__' == '__main__':
+@app.route('/Api')
+def api():
+      return render_template('api.html', year=copyright_year)
+
+@app.route('/Email')
+def email():
+      return render_template('email.html', year=copyright_year)
+
+@app.route('/SMS')
+def sms():
+      return render_template('sms.html', year=copyright_year)
+
+@app.route('/Success')
+def success():
+      return render_template('success.html', year=copyright_year)
+
+@app.route('/Privacy-Policy')
+def privacy_policy():
+      return render_template('privacy-policy.html', year=copyright_year)
+
+@app.route('/About-Me')
+def about_me():
+      return render_template('about-me.html', year=copyright_year)
+
+
+if __name__ == '__main__':
       app.run(debug=True)
