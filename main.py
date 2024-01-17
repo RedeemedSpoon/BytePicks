0cf765293db06132fcb2a9833a56e9723ca299e3
Ethan <QustoTemmo@protonmail.com>
2024-01-17 10:39:36 +0100

    Huge HTML advancement

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template, request
 from datetime import datetime
+import json
 
 app = Flask("__main__")
 copyright_year = datetime.now().year
+with open("./videos/daily.json") as file:
+    allVideos = json.load(file)
 
 
 @app.route("/")
 def home():
-    return render_template("home.html", year=copyright_year)
+    return render_template("home.html", year=copyright_year, videos=allVideos)
 
 
 @app.route("/Dashboard")
 def dashboard():
-    return render_template("api.html", year=copyright_year)
+    return render_template("dashboard.html", year=copyright_year, videos=allVideos)
 
 
 @app.route("/Api")
@@ -35,29 +38,19 @@ def explaination():
     return render_template("explaination.html", year=copyright_year)
 
 
-@app.route("/Success")
-def success():
-    return render_template("success.html", year=copyright_year)
-
-
 @app.route("/Privacy-Policy")
 def privacy_policy():
     return render_template("privacy-policy.html", year=copyright_year)
 
 
-@app.route("/About-Me")
-def about_me():
-    return render_template("about-me.html", year=copyright_year)
+@app.route("/About-Us")
+def about_us():
+    return render_template("about-us.html", year=copyright_year)
 
 
 @app.route("/Contact")
 def contact():
     return render_template("contact.html", year=copyright_year)
-
-
-def submit():
-    if request.method == "POST":
-        return render_template("success.html", year=copyright_year)
 
 
 if __name__ == "__main__":
